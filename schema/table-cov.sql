dda8922f45da8983297e517c566a42011ec6b12f
Panna Lipták <liptakpanna@gmail.com>
2021-09-10 12:08:12 +0200

    cov pk

--- schema/table-cov.sql
+++ schema/table-cov.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS cov(
     ena_run     varchar(20),       -- ENA ID
     pos         int,               -- Position in the sequence
-    coverage    int                -- Coverage in the given position
+    coverage    int,                -- Coverage in the given position
+    CONSTRAINT pk_cov PRIMARY KEY (ena_run,pos)
 );
